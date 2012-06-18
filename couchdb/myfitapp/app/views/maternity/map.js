31585cfcfd672a7ceeb1a7606092ce2cbfa4709e
Rueben Anderson <rueand713@fullsail.edu>
2012-06-18 17:06:48 -0500

    Added my db views
    Refactored the views as I am no longer using custom IDs. I had to add a
    "type" key to the routine objects, and I use that key to sort through
    the views. Because I need to use the _id and _rev keys I also added
    them to the list of data to be emitted.

---
+++ couchdb/myfitapp/app/views/maternity/map.js
@@ -0,0 +1,22 @@
+function(doc) {
+  if (doc.type[1] === "Maternity") {
+    emit(doc._id, {
+    	"_id": doc._id,
+    	"_rev": doc._rev,
+    	"type": doc.type,
+    	"name":doc.name,
+    	"location": doc.location,
+    	"routinetype": doc.routinetype,
+    	"sun": doc.sun,
+    	"mon": doc.mon,
+    	"tue": doc.tue,
+    	"wed": doc.wed,
+    	"thu": doc.thu,
+    	"fri": doc.fri,
+    	"sat": doc.sat,
+    	"repdur": doc.repdur,
+    	"notes": doc.notes,
+    	"date": doc.date
+    });
+  }
+};
